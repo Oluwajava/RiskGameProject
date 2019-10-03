88ed07235e79976a65f6cc8622c30653b714f8d2
SaiSukruth Nimmala <sai.sukruth@gmail.com>
2019-10-03 12:53:20 -0400

    sai_risk_commit1.2

---
+++ src/main/java/com/soen/riskgame/module/core/mapper/ContinentMapper.java
@@ -0,0 +1,12 @@
+package com.soen.riskgame.module.core.mapper
+
+
+public class ContinentMapper {
+    public ContinentMapper(){
+
+    }
+
+    protected ContinentDTO mapToContinent(String str){
+
+    }
+}
