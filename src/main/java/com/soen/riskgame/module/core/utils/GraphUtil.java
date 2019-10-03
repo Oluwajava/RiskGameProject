e260bdebcfc7074bf74277ef03dc8cac603772ca
SibilJoeKurian <Sibil.kurian@gmail.com>
2019-10-03 13:28:03 -0400

    comiting_GraphUtil.java

---
+++ src/main/java/com/soen/riskgame/module/core/utils/GraphUtil.java
@@ -0,0 +1,23 @@
+package com.soen.riskgame.module.core.utils;
+
+public class GraphUtil {
+    HashMap<Country, Boolean> visitedCountries;
+    Set<Country> countries;
+    boolean isConnectedGraph;
+
+    public boolean isGraphConnected(Set<Country> countrySet) {
+        boolean returnValue = false;
+        return returnValue;
+    }
+
+    public void validateGraoh() {
+
+    }
+
+    public boolean isConnectedGraph() {
+        boolean returnValue = false;
+        return returnValue;
+    }
+
+
+}
