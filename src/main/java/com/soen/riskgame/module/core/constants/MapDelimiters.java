f1a2ad3a77cbe9bf9afbbc7b927c0c88fddc927e
Mayokun Ige <igemayokun@gmail.com>
2019-10-03 14:00:43 -0400

    map

---
+++ src/main/java/com/soen/riskgame/module/core/constants/MapDelimiters.java
@@ -0,0 +1,19 @@
+package com.soen.riskgame.module.core.constants;
+
+public interface MapDelimiters {
+
+    String FILE_DELIMITER = "[files]";
+
+    String CONTINENT_DELIMETER = "[continents]";
+
+    String COUNTRY_DELIMETER = "[countries]";
+
+    String BORDER_DELIMETER = "[borders]";
+
+    String NEXT_LINE_DELIMETER = "\n";
+
+    String SPACE_DELIMITER = " ";
+
+    String CARRIAGE_DELIMITER = "\r";
+
+}
