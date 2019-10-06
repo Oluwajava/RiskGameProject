1f2b4aa89f4e163ee864138858e6a47e1635e1e2
Mayokun Ige <igemayokun@gmail.com>
2019-10-06 14:45:25 -0400

    javadoc

--- src/main/java/com/soen/riskgame/module/core/command/RemoveContinent.java
+++ src/main/java/com/soen/riskgame/module/core/command/RemoveContinent.java
@@ -1,38 +1,15 @@
 package com.soen.riskgame.module.core.command;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RemoveContinent {
-    String continentName; //to add modifier
 
-    public RemoveContinent() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+    private String continentName;
 
-    public RemoveContinent(String continentName) {
-        super();
-        this.continentName = continentName;
-    }
-
-    public boolean canEqual(Object object) {
-        // TODO Confirm
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        // TODO add code
-        return 0; //to modify
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        // TODO add code
-        return false; //to modify
-    }
-
-    @Override
-    public String toString() {
-        return "RemoveContinent [continentName=" + continentName + "]";
-    }
 
 }
