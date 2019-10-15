37f4cbb2d432d5ab5e805fabd87caa5515269e3e
h-munjal <h_munjal@encs.concordia.ca>
2019-10-15 19:46:56 -0400

    LoadMapCommand.java

---
+++ src/main/java/com/soen/riskgame/module/core/command/LoadMapCommand.java
@@ -0,0 +1,34 @@
+package com.soen.riskgame.module.core.command;
+
+import com.soen.riskgame.module.core.interfaces.Command;
+import com.soen.riskgame.module.core.model.MapData;
+
+public class LoadMapCommand implements Command {
+
+    /**
+     * Contains the Map Data
+     */
+    private MapData mapData;
+
+    private ShowMapListener showMapListener;
+
+    public LoadMapCommand(MapData mapData, ShowMapListener showMapListener) {
+        this.mapData = mapData;
+        this.showMapListener = showMapListener;
+    }
+
+    @Override
+    public void execute() {
+        if (mapData.getGameStarted()) {
+            showMapListener.showMap(mapData.buildStringGamePlayData());
+        } else {
+            showMapListener.showMap(mapData.buildStringData());
+        }
+    }
+
+    public interface ShowMapListener {
+
+        void showMap(String mapData);
+
+    }
+}
