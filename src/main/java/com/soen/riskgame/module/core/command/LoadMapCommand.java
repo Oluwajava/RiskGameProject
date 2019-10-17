6dbeffc0dd1afccfe359f3bcf79b980a16d61485
h-munjal <h_munjal@encs.concordia.ca>
2019-10-17 10:52:28 -0400

    JavaDoc for LoadMapCommand.java

--- src/main/java/com/soen/riskgame/module/core/command/LoadMapCommand.java
+++ src/main/java/com/soen/riskgame/module/core/command/LoadMapCommand.java
@@ -2,7 +2,10 @@ package com.soen.riskgame.module.core.command;
 
 import com.soen.riskgame.module.core.interfaces.Command;
 import com.soen.riskgame.module.core.model.MapData;
-
+/**
+ * Class LoadMapCommand implements the interface Command and is to support the functionality of loading a particular map
+ *
+ */
 public class LoadMapCommand implements Command {
 
     /**
@@ -10,13 +13,24 @@ public class LoadMapCommand implements Command {
      */
     private MapData mapData;
 
+    /**
+     * listener for show map
+     */
     private ShowMapListener showMapListener;
 
+    /**
+     * Method loadMapCommand provides the functionlity of loading the command
+     * @param mapData object of class mapData
+     * @param showMapListener object of ShowMapListener
+     */
     public LoadMapCommand(MapData mapData, ShowMapListener showMapListener) {
         this.mapData = mapData;
         this.showMapListener = showMapListener;
     }
 
+    /**
+     * Method execute is to execute a particular functionality
+     */
     @Override
     public void execute() {
         if (mapData.getGameStarted()) {
@@ -26,8 +40,17 @@ public class LoadMapCommand implements Command {
         }
     }
 
+    /**
+     * 
+     * Interface ShowMapListener
+     *
+     */
     public interface ShowMapListener {
 
+    	/**
+    	 * Method showMap
+    	 * @param mapData Details about a particular map
+    	 */
         void showMap(String mapData);
 
     }
