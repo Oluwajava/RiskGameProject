cbcfad1e0b75f55c79f7433bc9417cf042ee061f
h-munjal <h_munjal@encs.concordia.ca>
2019-10-17 11:12:19 -0400

    JavaDoc for the showMapCommand.java

--- src/main/java/com/soen/riskgame/module/core/command/ShowMapCommand.java
+++ src/main/java/com/soen/riskgame/module/core/command/ShowMapCommand.java
@@ -3,6 +3,11 @@ package com.soen.riskgame.module.core.command;
 import com.soen.riskgame.module.core.interfaces.Command;
 import com.soen.riskgame.module.core.model.MapData;
 
+/**
+ * Class ShowMapCommand implements the interface Command
+ * provides the functionality of the game command "show map"
+ *
+ */
 public class ShowMapCommand implements Command {
 
     /**
@@ -10,13 +15,25 @@ public class ShowMapCommand implements Command {
      */
     private MapData mapData;
 
+    /**
+     * instance of ShowMapListerner
+     */
     private ShowMapListener showMapListener;
 
+    /**
+     * Parameterized Constructor
+     * @param mapData Object of MapData
+     * @param showMapListener Show map listener
+     */
     public ShowMapCommand(MapData mapData, ShowMapListener showMapListener) {
         this.mapData = mapData;
         this.showMapListener = showMapListener;
     }
 
+    /**
+     * Method execute()
+     * calls the showMap function of ShowMapListerner 
+     */
     @Override
     public void execute() {
         if (mapData.getGameStarted()) {
@@ -26,8 +43,16 @@ public class ShowMapCommand implements Command {
         }
     }
 
+    /**
+     * 
+     * Interface ShowMapListener
+     *
+     */
     public interface ShowMapListener {
-
+    	/**
+    	 * Method showMap
+    	 * @param mapData data in the map
+    	 */
         void showMap(String mapData);
 
     }
