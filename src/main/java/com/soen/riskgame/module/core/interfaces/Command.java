13a1d4fe0b43990b5edfc0b0d903a29dbe805b42
h-munjal <h_munjal@encs.concordia.ca>
2019-10-18 11:41:04 -0400

    Interface Package

--- src/main/java/com/soen/riskgame/module/core/interfaces/Command.java
+++ src/main/java/com/soen/riskgame/module/core/interfaces/Command.java
@@ -1,7 +1,12 @@
 package com.soen.riskgame.module.core.interfaces;
-
+/**
+ * Interface Command which will be implemented to provide the functionality of all the commands present in this project
+ */
 public interface Command {
 
+	/**
+	 * Execute method to execute the commands
+	 */
     void execute();
 
 }
--- src/main/java/com/soen/riskgame/module/core/interfaces/FortificationAction.java
+++ src/main/java/com/soen/riskgame/module/core/interfaces/FortificationAction.java
@@ -1,8 +1,22 @@
 package com.soen.riskgame.module.core.interfaces;
 
+/**
+ * Interface to support the functionality of fortification phase
+ * @author hitan
+ *
+ */
 public interface FortificationAction {
 
+	/**
+	 * Method to Fortify a Country
+	 * @param fromCountry name of Country
+	 * @param toCountry name of New Country
+	 * @param num number
+	 */
     void fortifyCountry(String fromCountry, String toCountry, int num);
 
+    /**
+     * Method for not Fortifying anything
+     */
     void fortifyNone();
 }
