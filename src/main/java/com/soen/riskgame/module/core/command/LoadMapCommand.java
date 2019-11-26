a7073d381cc7aecbe97a508201ccb145cd6feba3
Sai <sai.sukruth@gmail.com>
2019-11-25 22:08:52 -0500

    mainjava1.4

--- src/main/java/com/soen/riskgame/module/core/command/LoadMapCommand.java
+++ src/main/java/com/soen/riskgame/module/core/command/LoadMapCommand.java
@@ -8,27 +8,49 @@ import com.soen.riskgame.module.core.utils.FileReader;
 import com.soen.riskgame.module.core.utils.MapDataUtil;
 import com.soen.riskgame.module.core.utils.MapParser;
 import com.soen.riskgame.module.core.utils.MapValidator;
-import sun.jvm.hotspot.debugger.MappedByteBufferDataSource;
 
 import java.io.IOException;
 
+/**
+ *  * * Class uses command pattern to abstract Editmap Command
+ *  *  * for the Map This class can be called by either the GUI or the command line to perform
+ *  *  * @see com.soen.riskgame.module.core.interfaces.Command
+ *  *  * @see <a href="https://refactoring.guru/design-patterns/command">Command Pattern Tutorial</a>
+ * @author Mayokun
+ */
 public class LoadMapCommand implements Command {
-
+    /**
+     * Name to the map file
+     */
     private String fileName;
-
+    /**
+     * object of the interface implemented
+     */
     private LoadMapListener loadMapListener;
 
+    /**
+     * Constructor for the MapEditor class Initializes mapIO object.
+     * @param fileName Name to the map file
+     * @param loadMapListener object of the interface implemented
+     */
     public LoadMapCommand(String fileName, LoadMapListener loadMapListener) {
         this.fileName = fileName;
         this.loadMapListener = loadMapListener;
     }
-
+    /**
+     * Method is inherited from the @see Command Interface
+     * which is used to abstract the execution of
+     * the Command
+     */
     @Override
     public void execute() {
         MapData data = MapDataUtil.loadMapFromFile(fileName);
         loadMapListener.loadMap(data);
     }
 
+    /**
+     * Interface representation for the class
+     */
     public interface LoadMapListener {
 
         void loadMap(MapData ma);
