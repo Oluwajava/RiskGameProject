a28a4a63df0a3aa94afb8b305a94b44663dbd6ed
Sai <sai.sukruth@gmail.com>
2019-11-26 21:38:21 -0500

    mainjava2.8

--- src/main/java/com/soen/riskgame/module/core/command_line/TokenType.java
+++ src/main/java/com/soen/riskgame/module/core/command_line/TokenType.java
@@ -2,6 +2,11 @@ package com.soen.riskgame.module.core.command_line;
 
 import lombok.Getter;
 
+/**
+ * Enum TokenType defining all the values of Token Type
+ * @author hitan
+ *
+ */
 public enum TokenType {
 
     ATOM(1, CommandType.PARAM), EDIT_CONTINENT(1, CommandType.COMMAND),
@@ -10,14 +15,27 @@ public enum TokenType {
     EDIT_MAP(1, CommandType.COMMAND), VALIDATEMAP(0, CommandType.COMMAND), LOAD_MAP(1, CommandType.COMMAND),
     GAME_PLAYER(1, CommandType.COMMAND), POPULATE_COUNTRIES(0, CommandType.COMMAND), PLACE_ARMY(1, CommandType.COMMAND),
     PLACE_ALL(0, CommandType.COMMAND), REINFORCE(2, CommandType.COMMAND), FORTIFY(3, CommandType.COMMAND),
-    NONE(0, CommandType.COMMAND);
+    NONE(0, CommandType.COMMAND), ATTACK(4, CommandType.COMMAND), DEFEND(1, CommandType.COMMAND),
+    ATTACK_MOVE(1, CommandType.COMMAND), ALL_OUT(0, CommandType.ACTION), NO_ATTACK(0, CommandType.ACTION),
+    EXCHANGE_CARDS(1, CommandType.COMMAND);
 
+    /**
+     * number of arguments
+     */
     @Getter
     private int numberOfArugments;
 
+    /**
+     * Object of the type CommandType
+     */
     @Getter
     private CommandType commandType;
 
+    /**
+     * parameterized constructor
+     * @param numberOfArugments number of arguments
+     * @param commandType type of command
+     */
     TokenType(int numberOfArugments, CommandType commandType) {
         this.numberOfArugments = numberOfArugments;
         this.commandType = commandType;
