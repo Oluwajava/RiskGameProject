6031b9778d308f59615f0630c8fa6b2fb56be847
h-munjal <h_munjal@encs.concordia.ca>
2019-11-28 14:36:02 -0500

    ContinentAction

--- src/main/java/com/soen/riskgame/module/core/interfaces/ContinentAction.java
+++ src/main/java/com/soen/riskgame/module/core/interfaces/ContinentAction.java
@@ -1,5 +1,8 @@
 package com.soen.riskgame.module.core.interfaces;
-
+/**
+ * Interface Continent to provide the basic functionality related to the entity Continent.
+ * @author Hitansh,Mayokun
+ */
 public interface ContinentAction {
 
     void addContinent(String name, int controlValue);
