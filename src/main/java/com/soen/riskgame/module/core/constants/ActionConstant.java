577448ee6734652852131b0ecf5b3fafe20d8f6f
h-munjal <h_munjal@encs.concordia.ca>
2019-11-28 14:16:14 -0500

    java

--- src/main/java/com/soen/riskgame/module/core/constants/ActionConstant.java
+++ src/main/java/com/soen/riskgame/module/core/constants/ActionConstant.java
@@ -1,5 +1,8 @@
 package com.soen.riskgame.module.core.constants;
-
+/**
+ * Interface that holds the action constants
+ * @author John
+ */
 public interface ActionConstant {
 
     String ADD = "-add";
