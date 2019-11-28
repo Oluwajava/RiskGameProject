23a627f559bbbd4633e6edd5284b021129c802ea
h-munjal <h_munjal@encs.concordia.ca>
2019-11-28 14:18:41 -0500

    java

--- src/main/java/com/soen/riskgame/module/core/constants/Constants.java
+++ src/main/java/com/soen/riskgame/module/core/constants/Constants.java
@@ -1,5 +1,8 @@
 package com.soen.riskgame.module.core.constants;
-
+/**
+ * Interface that holds the Constants
+ * @author John
+ */
 public interface Constants {
 
     String PREVIEW_PATH = "/maps/preview";
