b88b6fa6ddee8e722f75992f5692d220f9178b01
Mayokun Ige <igemayokun@gmail.com>
2019-11-27 22:26:44 -0500

    refactoring

--- src/main/java/com/soen/riskgame/module/core/interfaces/FortificationAction.java
+++
@@ -1,8 +0,0 @@
-package com.soen.riskgame.module.core.interfaces;
-
-public interface FortificationAction {
-
-    void fortifyCountry(String fromCountry, String toCountry, int num);
-
-    void fortifyNone();
-}
